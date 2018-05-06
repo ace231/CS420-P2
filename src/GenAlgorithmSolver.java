8584a5d1f1f5e5bd51942b753f9421e4fba7d5ed
alfredoceballos <ceballosalfredo231@gmail.com>
2018-05-05 21:08:56 -0700

    Some c

--- src/GenAlgorithmSolver.java
+++ src/GenAlgorithmSolver.java
@@ -1,8 +1,9 @@
+import java.lang.management.MemoryUsage;
 import java.util.*;
 
 public class GenAlgorithmSolver {
 
-    private static final int MAX_NUM_GENERATIONS = 7500;
+    private static final int MAX_NUM_GENERATIONS = 10000;
     private static List<Board> solutions;  // If solutions are found, save them here.
     private int currGenerations;
     private PriorityQueue<Board> population;
@@ -13,13 +14,13 @@ public class GenAlgorithmSolver {
 
 
     public GenAlgorithmSolver(Board[] initPopulation, double mutationProb) {
-        population = new PriorityQueue<>();
-        population.addAll(Arrays.asList(initPopulation)); // Take in board array, turn into a list for queue
-        solutions = new ArrayList<>();
-        currGenerations = 1;
-        populationSize = initPopulation.length;
-        nodesGenerated = 0;
+        this.population = new PriorityQueue<>(Arrays.asList(initPopulation)); // Take in board array, turn into a list for queue
+        this.solutions = new ArrayList<>();
+        this.currGenerations = 1;
+        this.populationSize = initPopulation.length;
+        this.nodesGenerated = 0;
         this.mutationProb = mutationProb;
+        printInfo();
     }
 
 
@@ -28,9 +29,7 @@ public class GenAlgorithmSolver {
     }
 
     public void solve() {
-        Iterator<Board> b;
-
-        // Loop through the process of selection, breeding, and mutation until the limit set is reached or
+        // Loop through the processes of selection, breeding, and mutation until the limit set is reached or
         // a solution is found
         do {
             // Check if the best candidate is the solution, since this is a priority queue, check the top.
@@ -41,69 +40,45 @@ public class GenAlgorithmSolver {
                 break;
             }
 
-            // This will become the new population. Since the population size has been set, the new generation will be
-            // of the same size and will consist of the children generated from this run through and some randomly
-            // selected candidates of the top 25%
+            // This will become the new population
             PriorityQueue<Board> newGeneration = new PriorityQueue<>();
 
-            for(int i = 0; i < populationSize; i++) {
-                // For my implementation 2 random parents will be selected from the top 25% of candidates rather than
-                // consistently drawing the top 2
-                Board parent1 = getRandBestParent(40, null);
-                Board parent2 = getRandBestParent(40, parent1);
+            for(int i = 0; i < populationSize / 2; i++) {
+                // Pick top 2 parents
+                Board parent1 = population.poll();
+                Board parent2 = population.poll();
+
+               // System.out.println("Population size: " + population.size() + " i: " + i);//delete
+
                 //Add them back into the population
                 newGeneration.add(parent1);
                 newGeneration.add(parent2);
 
                 // Breed the two parents to generate 2 children, this method also handles mutations
                 Board[] children = breed(parent1, parent2); // consists of 2 children
+
                 newGeneration.add(children[0]); // Add children to population
                 newGeneration.add(children[1]);
-
             }
 
-            this.population = newGeneration; // Set population to new population
+            this.population = newGeneration; // Set population to new population/generation
             currGenerations++; // Increment generations
         } while(currGenerations < MAX_NUM_GENERATIONS && population.peek().getFitness() != 0);
 
+        System.out.println("Max generations reached");
     }
 
 
-    // Randomly selects a parent from top given percentage of the population.
-    // A board object simulating a parent is passed as a parameter to make sure that the same parent is not
-    // picked twice. I did that just to appeal to my paranoia.
-    private Board getRandBestParent(int percentile, Board parent1) {
-        Random rand = new Random();
-        int limit = (int)(populationSize * (percentile / 100.0));  // Find limiting index
-        int randIndex = rand.nextInt(limit);  // Pick a random index between 0 and limit
-        Iterator<Board> itr = population.iterator();
-        Board newParent;
-
-        // Go through randIndex amount of elements to select a parent
-        int i = 0;
-        do {
-            newParent = itr.next();
-
-            if(newParent != parent1) {
-                i++;
-            }
-
-        } while (i < randIndex);
-
-        return newParent;
-    }
-
-
+    // Handles making 2 children from 2 parents and any mutations
     private Board[] breed(Board parent1, Board parent2) {
         Board[] children = new Board[2];
         Random rand = new Random();
 
-        // The mutation probability is a percentage, so we'll multiply it by 100 and generate a number between 0 and 100.
-        // If the number is between 0 and the mutation * 100, we'll throw a mutation into that child.
-        int chanceMutation = (int)(mutationProb * 100.0);
-        int chanceMutateC1 = rand.nextInt(chanceMutation);
-        int chanceMutateC2 = rand.nextInt(chanceMutation);
+        // Calculate the probability of either child being mutated
+        double chanceMutateC1 = rand.nextDouble();
+        double chanceMutateC2 = rand.nextDouble();
 
+        // We don't want the very first or very last indices as the cross over point
         int crossOverIndex = rand.nextInt(parent1.getSizeOfBoard() - 1);
         if(crossOverIndex == 0) {
             crossOverIndex++;
@@ -114,8 +89,9 @@ public class GenAlgorithmSolver {
         // child 2
         int[] p1Half1 = parent1.getPieceOfBoard(0, crossOverIndex);
         int[] p1Half2 = parent1.getPieceOfBoard(crossOverIndex, parent1.getSizeOfBoard());
+
         int[] p2Half1 = parent2.getPieceOfBoard(0, crossOverIndex);
-        int[] p2Half2 = parent1.getPieceOfBoard(crossOverIndex, parent1.getSizeOfBoard());
+        int[] p2Half2 = parent2.getPieceOfBoard(crossOverIndex, parent2.getSizeOfBoard());
 
         int[] boardChild1 = new int[parent1.getSizeOfBoard()]; // Should be p1Half1 + p2Half2
         int[] boardChild2 = new int[parent1.getSizeOfBoard()]; // Should be p2Half1 + p1Half2
@@ -128,8 +104,8 @@ public class GenAlgorithmSolver {
         }
 
         while(j < p1Half2.length) {
-            boardChild1[i] = p1Half2[j];
-            boardChild2[i] = p2Half2[j];
+            boardChild1[i] = p2Half2[j];
+            boardChild2[i] = p1Half2[j];
             i++;
             j++;
         }
@@ -138,10 +114,10 @@ public class GenAlgorithmSolver {
         children[1] = new Board(boardChild2);
 
         // Here we can mutate the children if need be
-        if(chanceMutateC1 < chanceMutation) {
+        if(chanceMutateC1 < mutationProb) {
             children[0].mutateBoard();
         }
-        if(chanceMutateC2 < chanceMutation) {
+        if(chanceMutateC2 < mutationProb) {
             children[1].mutateBoard();
         }
 
@@ -153,5 +129,22 @@ public class GenAlgorithmSolver {
     public void printInfo() {
         System.out.println("Size of population: " + population.size() + "\nCurrent generation: " + currGenerations +
                             "\npopulationSize: " + populationSize);
+    }
+
+    public void printPopulation() {
+        Board[] temp2 = new Board[population.size()];
+        temp2 = population.toArray(temp2);
+        for(int i = 0; i < 200; i++) {
+            Board temp = population.poll();
+            System.out.println(i+ ": " + temp.toString() + " cost " + temp.getFitness());
+        }
+
+
+        System.out.println("As array");
+
+        for(int i = 0; i < temp2.length; i++) {
+            System.out.println(i + ": " + temp2[i].toString() + " cost " + temp2[i].getFitness());
+        }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     // Create new population every run for genetic alg, population size can vary
     // Run > 400 tests
     // Check for solution set at beginning of loop for simulated annealing
     public static void main(String[] args) {
-        // Generate initial population of size 100
-        Board[] initPopulation = new Board[1000];
-        for(int i = 0; i < 1000; i++) {
-            initPopulation[i] = new Board(); // Default constructor makes a random board of size 21
-        }
+        Board[] initPopulation = new Board[250];
+        GenAlgorithmSolver genetic = null;
 
-        System.out.println(initPopulation[999]);
+        //for(int iterations = 0; iterations < 500; iterations++) {
+            for (int i = 0; i < 250; i++) {
+                initPopulation[i] = new Board(); // Default constructor makes a random board of size 21
+            }
 
-        GenAlgorithmSolver genetic = new GenAlgorithmSolver(initPopulation, 0.1);
-        genetic.solve();
-        ArrayList<Board> sols = (ArrayList) genetic.getSolutions();
-        for(Board b: sols) {
+            // Initial population of size 200 and a mutation probably of 0.1, or 10%
+            genetic = new GenAlgorithmSolver(initPopulation, 0.01);
+            genetic.solve();
+       // }
+
+        List<Board> sols = genetic.getSolutions();
+        for(Board b : sols) {
             System.out.println(b.toString());
         }
     }
